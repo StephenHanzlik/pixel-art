536677b7903be1a2296c6890238c86af2a84fce4
Stephen Hanzlik <stephenhanzlik@Stephens-MacBook-Pro.local>
2016-10-16 11:52:54 -0600

    filled out colArr with nice spread of colors.  fixed formating on palette to flex

--- pixelArt.js
+++ pixelArt.js
@@ -4,26 +4,26 @@
 
 var paletteStore = document.getElementsByClassName("paletteStore");
 
-var palette1 = document.getElementsByClassName("palcolorAquamarine");
-palette1[0].addEventListener("click", function() {
-  paletteStore[0].style.backgroundColor = 'aquamarine';
-});
-var palette2 = document.getElementsByClassName("palcolorDarkGoldenRod");
-palette2[0].addEventListener("click", function() {
-  paletteStore[0].style.backgroundColor = 'darkgoldenrod';
-});
-var palette3 = document.getElementsByClassName("palcolorDarkOliveGreen");
-palette3[0].addEventListener("click", function() {
-  paletteStore[0].style.backgroundColor = 'darkolivegreen';
-});
-var palette4 = document.getElementsByClassName("palcolorDarkSalmon");
-palette4[0].addEventListener("click", function() {
-  paletteStore[0].style.backgroundColor = 'darksalmon';
-});
+// var palette1 = document.getElementsByClassName("palcolorAquamarine");
+// palette1[0].addEventListener("click", function() {
+//   paletteStore[0].style.backgroundColor = 'aquamarine';
+// });
+// var palette2 = document.getElementsByClassName("palcolorDarkGoldenRod");
+// palette2[0].addEventListener("click", function() {
+//   paletteStore[0].style.backgroundColor = 'darkgoldenrod';
+// });
+// var palette3 = document.getElementsByClassName("palcolorDarkOliveGreen");
+// palette3[0].addEventListener("click", function() {
+//   paletteStore[0].style.backgroundColor = 'darkolivegreen';
+// });
+// var palette4 = document.getElementsByClassName("palcolorDarkSalmon");
+// palette4[0].addEventListener("click", function() {
+//   paletteStore[0].style.backgroundColor = 'darksalmon';
+// });
 
 // Loop through array of colors and create a palatte:
-
-var colArr = ['goldenrod', 'dimgray', 'darkslateblue', 'darkseagreen', 'indigo', 'lavender', 'khaki'];
+var colArr = ['#f44842', '#db4b27', '#db6927', '#db7e27', '#db9f27', '#dbb127', '#dbc027', '#dbd227', '#cfdb27', '#abdb27', '#84db27', '#54db27', '#27db30', '#27db75', '#27db90', '#27dbcf', '#27c0db', '#275adb', '#272adb', '#6c27db', '#8727db', '#9927db', '#bd27db', '#db27db', '#db27b1','#840976', '#840959', '#600722', '#2b020e', '#070001', '#ffffff'];
+// var colArr = ['goldenrod', 'dimgray', 'darkslateblue', 'darkseagreen', 'indigo', 'lavender', 'khaki','darksalmon', 'darkolivegreen', 'darkgoldenrod', 'aquamarine'];
 
 for (var x = 0; x < colArr.length; x++) {
   var palette = document.getElementsByClassName("palette");
