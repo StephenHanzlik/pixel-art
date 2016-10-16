f61f44b2ac9b2a3ba001d8c03bb2311d76389c0c
Stephen Hanzlik <stephenhanzlik@Stephens-MacBook-Pro.local>
2016-10-16 15:24:24 -0600

    fixed formatting in palette and fixed formating of text in palette

--- pixelArt.js
+++ pixelArt.js
@@ -7,7 +7,8 @@ paletteStore[0].style.backgroundColor = 'white';
 
 // Loop through array of colors and create a palatte:
 
-var colArr = ['#f44842', '#db4b27', '#db6927', '#db7e27', '#db9f27', '#dbb127', '#dbc027', '#dbd227', '#cfdb27', '#abdb27', '#84db27', '#54db27', '#27db30', '#27db75', '#27db90', '#27dbcf', '#27c0db', '#275adb', '#272adb', '#6c27db', '#8727db', '#9927db', '#bd27db', '#db27db', '#db27b1','#840976', '#840959', '#600722', '#2b020e', '#070001', '#ffffff'];
+var colArr = ['#f44842', '#db4b27', '#db6927', '#db7e27', '#db9f27', '#dbb127', '#dbc027', '#dbd227', '#cfdb27', '#abdb27', '#84db27', '#54db27', '#27db30', '#27db75', '#27db90', '#27dbcf', '#27c0db', '#275adb', '#272adb', '#6c27db', '#8727db', '#9927db', '#bd27db', '#db27db', '#db27b1'];
+// '#840976', '#840959', '#600722', '#2b020e', '#070001', '#ffffff'];
 
 var palette = document.getElementsByClassName("palette");
 
@@ -17,10 +18,10 @@ for (var x = 0; x < colArr.length; x++) {
   palCreate.style.height = '15%';
   palCreate.style.width = '2.5%';
   palCreate.style.padding = '1%';
-  // palCreate.style.marginLeft = '40px';
-  // palCreate.style.marginTop = '20px';
+  palCreate.style.marginLeft = '10px';
+  palCreate.style.marginTop = '10px';
   palCreate.style.borderRadius = '50%';
-  palCreate.style.border = '1px solid #e0e0e0';
+  // palCreate.style.border = '1px solid #e0e0e0';
   palette[0].appendChild(palCreate);
   palCreate.addEventListener("click", function() {
     paletteStore[0].style.backgroundColor = event.target.style.backgroundColor;
@@ -28,7 +29,7 @@ for (var x = 0; x < colArr.length; x++) {
 };
 
 //create and append divs to fill out canvas:
-//i < 2808  &&  height 42px
+
 for (var i = 0; i < 4422; i++) {
  var canvas = document.getElementsByClassName("canvas");
  var pixCreate = document.createElement("div");
@@ -45,9 +46,10 @@ for (var i = 0; i < 4422; i++) {
 //append 'current color' text to pallette:
 
 var currentColTXT = document.createElement("p");
-currentColTXT.innerText = 'current color > ';
-currentColTXT.style.fontSize = '500%';
-currentColTXT.style.paddingBottom = '500px';
+currentColTXT.innerText = 'CURRENT COLOR > ';
+currentColTXT.style.fontSize = '150%';
+currentColTXT.style.marginLeft = '20px';
+currentColTXT.style.alignSelf = 'flex-start';
 palette[0].appendChild(currentColTXT);
 
 //create a div & house current color pic:
